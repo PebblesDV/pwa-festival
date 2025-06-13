5018c10c0f9d63748cdbaaf52754046ff0bf1b7b
PebblesDV <pebblesdevries@gmail.com>
2025-06-13 13:31:20 +0200

    artist detail page and lineup add ons

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,10 +2,21 @@ export default function Footer() {
   return (
     <footer className="flex h-20 w-full justify-center items-center px-5 py-4 bg-red-500 mt-auto">
       <div className="flex items-center justify-between gap-10">
-        <i className="ri-home-2-line text-5xl text-white rounded-full"></i>
-        <i className="ri-information-line text-5xl text-white rounded-full"></i>{" "}
-        <i className="ri-music-fill text-5xl text-white rounded-full"></i>
-        <i className="ri-map-pin-line text-5xl text-white rounded-full"></i>
+        <a href="/">
+          <i className="ri-home-2-line text-5xl text-white rounded-full"></i>
+        </a>
+
+        <a href="/info">
+          <i className="ri-information-line text-5xl text-white rounded-full"></i>{" "}
+        </a>
+
+        <a href="/lineup">
+          <i className="ri-music-fill text-5xl text-white rounded-full"></i>
+        </a>
+
+        <a href="/map">
+          <i className="ri-map-pin-line text-5xl text-white rounded-full"></i>
+        </a>
       </div>
     </footer>
   );
